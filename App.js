9b12361594e70bff271f6d8104db2010dde02ad0
xanderssamoth <124665689+xsamtech@users.noreply.github.com>
2024-06-05 13:27:21 +0100

    n

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * @author Xanders
+ * @see https://team.xsamtech.com/xanderssamoth
+ */
+import React from 'react';
+import { View } from 'react-native';
+import Routes from './routes';
+
+const App = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Routes />
+        </View>
+    );
+};
+
+export default App;
