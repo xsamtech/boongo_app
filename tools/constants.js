534d4d049fc3e06fa1beaf17c821b38db618fbc3
xanderssamoth <124665689+xsamtech@users.noreply.github.com>
2024-06-09 07:58:49 +0100

    Update constants.js

---
+++ tools/constants.js
@@ -0,0 +1,33 @@
+/**
+ * @author Xanders
+ * @see https://team.xsamtech.com/xanderssamoth
+ */
+export const COLORS = {
+    danger: '#db3337',
+    primary: '#3a7ced',
+    success: '#2ba14d',
+    warning: '#ecad26',
+    light: '#f4f3f6',
+    white: '#fff',
+    black: '#000',
+    iconColor: '#eaebef'
+};
+
+export const ICON_SIZE = {
+    s1: 25,
+    s2: 23,
+    s3: 21,
+    s4: 19,
+    s5: 16,
+    s6: 14,
+};
+
+export const TEXT_SIZE = {
+    title: 22,
+    secondary: 14
+};
+
+export const PADDING = {
+    horizontal: 15,
+    vertical: 10
+};
